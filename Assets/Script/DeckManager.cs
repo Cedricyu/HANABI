f6ae5f33b12311a637bd7bdc664520423e316390
Cedricyu <86541569+Cedricyu@users.noreply.github.com>
2023-04-01 21:32:22 +0800

    add card

--- Assets/Script/DeckManager.cs
+++ Assets/Script/DeckManager.cs
@@ -5,42 +5,63 @@ using UnityEngine;
 public class DeckManager : MonoBehaviour
 {
     // Start is called before the first frame update
-    [SerializeField] List<CardData> Deck;
+    [SerializeField] List<Card> Deck = new List<Card>();
+    public static DeckManager DM;
 
-    public List<CardData> discard;
+    public List<Card> discard;
 
-    public CardData Cards;
+    public Card Card;
+
+
+    public List<string> colors_;
+
+    Transform parentTransform ;
 
     void Start()
     {
+        parentTransform = this.transform;
+        DM = this;
+
+        colors_.Add("red");
+        colors_.Add("blue");
+        colors_.Add("yellow");
+        colors_.Add("white");
+        colors_.Add("green");
         generateCards();
     }
 
     // Update is called once per frame
     private void generateCards()
     {
-        for (int i = 0; i < 5; i++)
+        for (int i = 0; i < colors_.Count ; i++)
         {
-            var card = ScriptableObject.CreateInstance<CardData>();
-            card.Color = "red";
-            card.Number = i + 1;
-            Deck.Add(card);
+           for(int j=0 ; j <5 ; j++ ){
+                Card newCard = new Card(colors_[i],i);
+                GameObject newCardObject = new GameObject("New Card");
+                newCardObject.transform.SetParent(parentTransform);
+                SpriteRenderer CardSprite = newCardObject.AddComponent<SpriteRenderer>();
+
+                Card CardObject = newCardObject.AddComponent<Card>();
+                CardObject.GetComponent<Card>().CopyFrom(newCard);
+
+                Sprite mySprite = Card.sprites[i];
+                CardSprite.sprite = mySprite;
+
+                Deck.Add(CardObject);
+           }
         }
-        for (int i = 0; i < 5; i++)
-        {
-            Deck.Add(new CardData("blue",i));
-        }
+        
     }
 
     public void DrawCard(){
-        if(Deck.Count <=0 )
-            return;
+        // if(Deck.Count <=0 )
+        //     return;
 
-        CardData randomCard = Deck[Random.Range(0, Deck.Count)];
+        // CardData randomCard = Deck[Random.Range(0, Deck.Count)];
 
 
-        discard.Add(randomCard);
-        Deck.Remove(randomCard);
+        // discard.Add(randomCard);
+        // Deck.Remove(randomCard);
     }
 
 }
