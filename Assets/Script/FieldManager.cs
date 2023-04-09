ec9bcc2412eb709adec5e0a6d2b601ffc26bac62
Cedricyu <86541569+Cedricyu@users.noreply.github.com>
2023-04-09 20:58:43 +0800

    checkcard

---
+++ Assets/Script/FieldManager.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FieldManager : MonoBehaviour
+{
+    [SerializeField] List<redCard> redCards;
+    [SerializeField] List<whiteCard> whiteCards;
+    [SerializeField] List<blueCard> blueCards;
+    [SerializeField] List<greenCard> greenCards;
+    [SerializeField] List<yellowCard> yellowCards;
+
+    public bool canPlay(Card playCard){
+        
+
+    }
+}
