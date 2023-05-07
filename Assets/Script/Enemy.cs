b03942cae9fcf9b9cd9aba3bfed5052022e6293b
Cedricyu <86541569+Cedricyu@users.noreply.github.com>
2023-05-07 22:34:07 +0800

    add enemy

---
+++ Assets/Script/Enemy.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] List<Transform> transforms_;
+    [SerializeField] Player player_;
+
+    public static Enemy instance_; 
+   
+    private void Start()
+    {
+        instance_ = this;
+    }
+
+    private void FixedUpdate()
+    {
+        if(player_!=null)
+            for (int i = 0; i < player_.Hands.Count; i++)
+            {
+                Card tmp = player_.Hands[i];
+                tmp.transform.position = transforms_[i].position;
+            }
+    }
+
+    public void AddPlayer(Player p)
+    {
+        player_ = p;
+    }
+}
