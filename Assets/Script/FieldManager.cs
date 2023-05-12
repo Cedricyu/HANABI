6ab665b7faad5a95d52e30435218a16a9f880817
Cedricyu <86541569+Cedricyu@users.noreply.github.com>
2023-05-12 21:58:20 +0800

    add play button

--- Assets/Script/FieldManager.cs
+++ Assets/Script/FieldManager.cs
@@ -1,59 +1,89 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
 
-public class FieldManager : MonoBehaviour
+public class FieldManager : MonoBehaviourPun
 {
     public static FieldManager Instance;
-    [SerializeField] List<redCard> redCards;
-    [SerializeField] List<whiteCard> whiteCards;
-    [SerializeField] List<blueCard> blueCards;
-    [SerializeField] List<greenCard> greenCards;
-    [SerializeField] List<yellowCard> yellowCards;
+    private List<List<Card>> field = new List<List<Card>>();
+    [SerializeField] List<Card> redCards;
+    [SerializeField] List<Card> whiteCards;
+    [SerializeField] List<Card> blueCards;
+    [SerializeField] List<Card> greenCards;
+    [SerializeField] List<Card> yellowCards;
+    [SerializeField] List<Card> discardPile;
 
     [SerializeField] List<Transform> fields;
     [SerializeField] Transform discard;
 
     public void Start(){
         Instance = this;
+        field.Add(redCards);
+        field.Add(blueCards);
+        field.Add(whiteCards);
+        field.Add(greenCards);
+        field.Add(yellowCards);
+    }
+    private void FixedUpdate()
+    {
+        for(int i = 0; i < field.Count; i++)
+        {
+            foreach(Card c in field[i])
+            {
+                c.transform.position = fields[i].position;
+            }
+        }
     }
 
     public bool canPlay(Card playCard){
         Debug.Log(playCard);
         Debug.Log("color "+playCard.getColor()+" number "+playCard.getNumber());
-        if (playCard.GetType()==typeof(redCard) && (redCards.Count+1 == playCard.getNumber()) ){
+        if (playCard is redCard && (redCards.Count+1 == playCard.getNumber()) ){
             Debug.Log("red");
-            redCards.Add((redCard)playCard);
-            playCard.transform.position = fields[0].position;
+            PhotonView.Get(this).RPC("ReveiveData", RpcTarget.All, playCard.getId(), 0);
         }
-        else if (playCard.GetType()==typeof(blueCard) && (blueCards.Count+1 == playCard.getNumber()) )
+        else if (playCard is blueCard && (blueCards.Count+1 == playCard.getNumber()) )
         {
             Debug.Log("blue");
-            blueCards.Add((blueCard)playCard);
-            playCard.transform.position = fields[1].position;
+            PhotonView.Get(this).RPC("ReveiveData", RpcTarget.All, playCard.getId(), 1);
         }
-        else if (playCard.GetType()==typeof(whiteCard) && (whiteCards.Count+1 == playCard.getNumber()) )
+        else if (playCard is whiteCard && (whiteCards.Count+1 == playCard.getNumber()) )
         {
             Debug.Log("white");
-            whiteCards.Add((whiteCard)playCard);
-            playCard.transform.position = fields[2].position;
+            PhotonView.Get(this).RPC("ReveiveData", RpcTarget.All, playCard.getId(), 2);
         }
-        else if (playCard.GetType()==typeof(greenCard) && (greenCards.Count+1 == playCard.getNumber()) )
+        else if (playCard is greenCard && (greenCards.Count+1 == playCard.getNumber()) )
         {
             Debug.Log("green");
-            greenCards.Add((greenCard)playCard);
-            playCard.transform.position = fields[3].position;
+            PhotonView.Get(this).RPC("ReveiveData", RpcTarget.All, playCard.getId(), 3);
         }
         else if (playCard.GetType()==typeof(yellowCard) && (yellowCards.Count+1 == playCard.getNumber()) )
         {
             Debug.Log("yellow");
-            yellowCards.Add((yellowCard)playCard);
-            playCard.transform.position = fields[4].position;
+            PhotonView.Get(this).RPC("ReveiveData", RpcTarget.All, playCard.getId(), 4);
         }
         else{
-            playCard.transform.position = discard.position;
+            PhotonView.Get(this).RPC("ReveiveData", RpcTarget.All, playCard.getId(), 5);
             return false;
         }
         return true;
+    }
+
+
+    [PunRPC]
+    public void ReveiveData(int id,int pos)
+    {
+        Card tmp = GameManager.instance_.GetCardbyId(id);
+        if (pos > 4)
+        {
+            tmp.transform.position = discard.position;
+            discardPile.Add(tmp);
+        }
+        else
+        {
+            tmp.transform.position = fields[pos].position;
+            field[pos].Add(tmp);
+        }
     }
 }
