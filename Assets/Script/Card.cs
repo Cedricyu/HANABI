2adc154ff5d1a68ebc5e16ddcf62dea7a66aaa30
test <ann0918089105@gmail.com>
2023-05-21 14:52:09 +0800

    1

--- Assets/Script/Card.cs
+++ Assets/Script/Card.cs
@@ -1,56 +1,60 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
-public class Card : MonoBehaviour //public abstract class Card : MonoBehaviour
+public abstract class Card : MonoBehaviour
 {
     // Start is called before the first frame update
     [SerializeField] private int id_;
-    [SerializeField] protected int number_ ;
-    [SerializeField] protected string color_ ;
-    [SerializeField] public GameObject image;
-    
+    [SerializeField] protected int number_;
+    [SerializeField] protected string color_;
+
+
     DeckManager dm;
-    
-    protected virtual void Start(){
+    PlayerSystem player_;
+
+    protected virtual void Start()
+    {
         dm = DeckManager.Instance;
+
+    }
+    public void SetPlayer(PlayerSystem playerSystem_)
+    {
+        player_ = playerSystem_;
     }
 
-    public int getNumber(){
+    public int getNumber()
+    {
         return number_;
     }
 
-    public string getColor(){
+    public string getColor()
+    {
         return color_;
     }
     public int getId()
     {
         return id_;
     }
-   
-    public void cardInit(int n,int id){
+
+    public void cardInit(int n, int id)
+    {
         number_ = n;
         id_ = id;
         return;
     }
-    public void OnMouseDown()
+    void OnMouseDown()
     {
+        player_ = GetComponent<PlayerSystem>();
         // Destroy the gameObject after clicking on it
+        // transform.position += Vector3.up * 5;
         Debug.Log("clicked !");
-        GernerateHints();
-        //Debug.Log(color_);
+        player_.play_id = id_;
         //Destroy(gameObject);
     }
 
-   public virtual void GernerateHints()
+    public virtual void GernerateHints()
+    {
 
-   {
-    //if(color_==white)
-        //生成白色的object
-        //image.GetComponent<Renderer>().material.color = new Color32(255,255,225,100);
-        Debug.Log(color_);
-        Debug.Log(number_);
-   }
-
+    }
 }
