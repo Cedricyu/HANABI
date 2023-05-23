509fa27bcd21c40d86e967d21f9ffb4104ae2948
test <ann0918089105@gmail.com>
2023-05-23 15:33:48 +0800

    blue_hint_ok

--- Assets/Script/Card/blueCard.cs
+++ Assets/Script/Card/blueCard.cs
@@ -2,36 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using UnityEngine.AddressableAssets;
-using UnityEngine.ResourceManagement.AsyncOperations;
 
 public class blueCard : Card
 {
-    GameObject card_hint_color;
-    private Sprite blueSquareSprite; // 蓝色正方形的纹理
-    private RectTransform canvasRectTransform;
-    private Image imageComponent;
+    private GameObject card_hint_color;
     private Transform cardHintTransform;
-    private Canvas canvasComponent;
-    private Vector3 previousPosition; // 上一帧的位置
     private SpriteRenderer sr;
     private Sprite sprite;
-     
     // Start is called before the first frame update
     protected override void Start()
     
     {
 
-        base.Start(); // Call the base class Start() method first
+        base.Start();    // Call the base class Start() method first
         color_ = "Blue"; // Initialize the color_ variable in the derived class
-        card_hint_color=new GameObject("test_Blue");//////它要給誰
-        //card_hint_color.transform.position = new Vector3((float)(transform.position.x),(float) (transform.position.y + 0.08) , (float)(transform.position.z));
-        //card_hint_color.SetActive(false);
+        card_hint_color=new GameObject("Blue");
+        card_hint_color.SetActive(false);
         card_hint_color.transform.parent = transform;
+
         sr = card_hint_color.AddComponent<SpriteRenderer>() as SpriteRenderer;
-        //sr.color = new Color(0.9f, 0.9f, 0.9f, 1.0f);
-        //sprite = Resources.Load<Sprite> ("Assets/Sprites/blue.png");
-        sprite = Resources.Load<Sprite>("blue.png");
+        card_hint_color.transform.localScale = new Vector3(transform.localScale.x*40,  transform.localScale.y*70, transform.localScale.z*2);;
+        sprite = Resources.Load<Sprite>("color");//放入color.png
+        sr.color = new Color(0.0f, 0.0f, 1.0f, 1.0f);//給顏色
+       
         sr.sprite = sprite;
          
     }
@@ -51,7 +44,7 @@ public class blueCard : Card
 
     private void OnCardPositionChanged()
     {
-        //card_hint_color.SetActive(false);
+        card_hint_color.SetActive(true);
         
     }
 
