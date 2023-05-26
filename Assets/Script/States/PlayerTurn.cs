d0989bc1f01990d3c5f5bc4dd6431c0359406750
Yhgtw <ann0918089105@gmail.com>
2023-05-26 22:20:15 +0800

    05266666666

--- Assets/Script/States/PlayerTurn.cs
+++ Assets/Script/States/PlayerTurn.cs
@@ -1,9 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
+
 [System.Serializable]
-public class PlayerTurn : State
+public class PlayerTurn : State 
 {
     public PlayerTurn(PlayerSystem player) : base(player) { }
 
@@ -29,21 +29,6 @@ public class PlayerTurn : State
         }
         player_.SetState(new EndTurn(player_));
         Debug.Log("player turn to end state");
-        yield return new WaitForSeconds(1f);
-    }
-    public override IEnumerator DiscardCard()
-    {
-        if (!player_.Discard())
-        {
-            // TODO player_.SetState()
-        }
-        player_.SetState(new EndTurn(player_));
-        Debug.Log("player turn to end state");
-        yield return new WaitForSeconds(1f);
-    }
-    public override IEnumerator End()
-    {
-        SceneManager.LoadScene("GameOver");
         yield return new WaitForSeconds(1f);
     }
 
